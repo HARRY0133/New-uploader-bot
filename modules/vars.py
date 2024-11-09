f59604776e64ed3061dbc629987a15d14e3a2227
HARRY0133 <excuseboy13@gmail.com>
2024-11-09 17:37:13 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27862677"
-api_hash = "e343ce2c81b2b6c2c0d6bee58284e3bd"
-bot_token = "7063024370:AAFHuTYE6bOwjI5s3Nodv2mi8JGctq15nu0"
+api_id = "23253271"
+api_hash = "83ea976f17a307e2ff5b76823305052f"
+bot_token = "7700281692:AAHzb8PK9KlVwnrOZg76_DaeQCEKbDzdoCI"
